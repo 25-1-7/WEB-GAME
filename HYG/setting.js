e273c32211877d88d2cbf500508392e483a78db3
dudrms0811 <sio0811@naver.com>
2025-06-05 15:09:55 +0900

    setting완성본
    볼륨 크기 조절 추가

---
+++ HYG/setting.js
@@ -0,0 +1,39 @@
+$(document).ready(function () {
+  let bgmAudio = new Audio();
+  let sfxEnabled = true;
+  let sfxVolume = 0.5;
+
+  function applySettings() {
+    window.userBallColor = $('#ballColor').val();
+    const bgmFile = $('#bgmSelect').val();
+    const bgmToggle = $('#bgmToggle').is(':checked');
+    const bgmVolume = parseFloat($('#bgmVolume').val());
+
+    bgmAudio.src = bgmFile;
+    bgmAudio.loop = true;
+    bgmAudio.volume = bgmVolume;
+    if (bgmToggle) bgmAudio.play();
+    else bgmAudio.pause();
+
+    sfxEnabled = $('#sfxToggle').is(':checked');
+    sfxVolume = parseFloat($('#sfxVolume').val());
+  }
+
+  function playSFX(soundFile) {
+    if (!sfxEnabled) return;
+    const sfx = new Audio(soundFile);
+    sfx.volume = sfxVolume;
+    sfx.play();
+  }
+
+  $('#toggleSettingsBtn').on('click', function () {
+    $('#settings').toggle();
+  });
+
+  $('#closeSettingsBtn').on('click', function () {
+    $('#settings').hide();
+  });
+
+  // applySettings 함수를 외부에서도 쓸 수 있게 만듦
+  window.applySettings = applySettings;
+});
